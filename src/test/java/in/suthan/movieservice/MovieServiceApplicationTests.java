d11c0b87469891c18950950ba823ea79d5a43853
Suthan SM <suthantippu@gmail.com>
2022-01-08 14:12:28 +0530

    Initial project creation

---
+++ src/test/java/in/suthan/movieservice/MovieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.suthan.movieservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
